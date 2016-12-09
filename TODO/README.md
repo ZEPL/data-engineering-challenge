aeed1d9abee40ed8e0e5a4d172140675dadae2f6
Youngchan Kim <ovekyc@gmail.com>
2016-12-09 23:39:16 +0900

    Build Project Skeleton

---
+++ TODO/README.md
@@ -0,0 +1,7 @@
+# TODO-challenge
+
+Youngchan Kim (ovekyc)
+
+Maven 3.3 or later version required
+
+    mvn exec:java -Dexec.args="PORT_NUMBER"
---
+++ TODO/src/main/java/com/ychan/Server.java
@@ -0,0 +1,11 @@
+package com.ychan;
+
+public class Server {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    for(String str : args) {
+      System.out.println(str);
+    }
+    System.out.println("Hello World!2");    
+  }
+}
