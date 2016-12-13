f0872bd36ea14a75b96047c66c280d598274cc21
JihoonKwon <hsboykjh@gmail.com>
2016-12-13 16:22:58 +1100

    model added
    todo, todos model added

---
+++ src/main/java/com/jihoon/rest/model/todo.java
@@ -0,0 +1,41 @@
+package com.jihoon.rest.model;
+
+/**
+ * Created by USER on 2016-12-13.
+ */
+public class todo {
+
+    private String id;
+    private String name;
+    private String created;
+
+    public todo(String id, String name, String created) {
+        this.id = id;
+        this.name = name;
+        this.created = created;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
---
+++ src/main/java/com/jihoon/rest/model/todos.java
@@ -0,0 +1,16 @@
+package com.jihoon.rest.model;
+
+import java.util.List;
+
+public class todos {
+
+    List<todo> todos;
+
+    public List<todo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<todo> todos) {
+        this.todos = todos;
+    }
+}
