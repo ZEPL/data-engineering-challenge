b28730989109314705523db1ad6c8594f13925ce
JihoonKwon <hsboykjh@gmail.com>
2016-12-13 20:44:39 +1100

    logback added
    1.logback library added
    2.logback config added

--- src/main/java/com/jihoon/app.java
+++ src/main/java/com/jihoon/app.java
@@ -1,19 +1,29 @@
 package com.jihoon;
 
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 import com.google.inject.servlet.GuiceFilter;
+import com.jihoon.config.configModule;
+import com.jihoon.config.configuration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import java.util.EnumSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class app {
 
+    final static Logger logger = LoggerFactory.getLogger(app.class);
+
     public static void main(String[] args) throws Exception {
+
+        logger.info("Msg #1");
+        logger.warn("Msg #2");
+        logger.error("Msg #3");
+        logger.debug("Msg #4");
 
         Injector injector = Guice.createInjector( Stage.PRODUCTION, new configModule());
 
