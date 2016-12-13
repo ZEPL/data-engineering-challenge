c67871569b646c399a24c9533c80fae68201c1bc
Youngchan Kim <ovekyc@gmail.com>
2016-12-14 00:48:05 +0900

    Fix typo
    Add todoid field to Task

--- TODO/src/main/java/com/ychan/dto/Task.java
+++ TODO/src/main/java/com/ychan/dto/Task.java
@@ -7,33 +7,40 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-public class Task implements Dto{
+public class Task implements Dto {
+  public static String NOT_DONE = "NOT_DONE";
+  public static String DONE = "DONE";
   public String id;
   public String name;
   public String description;
   public String status;
+  public String todoId;
   @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS")
   public Date created;
 
   public Task(@JsonProperty("name") String name, 
       @JsonProperty("description") String description,
-      @JsonProperty("status") String status) {
+      @JsonProperty("status") String status,
+      @JsonProperty("todoId") String todoId) {
     this.id = makeId(this.getClass().getName());
     this.created = new Date();
     this.name = name;
     this.description = description;
     this.status = status;
+    this.todoId = todoId;
   }
 
   public String getId() { return id; }
   public String getName() { return name; }
   public String getDescription() { return description; }
-  public String getStaus() { return status; }
+  public String getStatus() { return status; }
+  public String getTodoId() { return todoId; }
   public Date getCreated() { return created; }
 
   public void setId(String id) { this.id = id; }
   public void setName(String name) { this.name = name; }
   public void setDescription(String description) { this.description = description; }
   public void setStatus(String status) { this.status = status; }
+  public void setTodoId(String todoId) { this.todoId = todoId; }
   public void setCreated(Date created) { this.created = created; }
 }
