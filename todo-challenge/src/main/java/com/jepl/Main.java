a5e27b4344e85df710de611a9b7d407ad2798d10
james.boo <boojongmin@daum.net>
2016-12-15 13:26:42 +0900

    update Main.java for runnable without web.xml. -> I met null point exception when find web.xml.
    fix by using servlet 3.0 spec's javaconfig

--- todo-challenge/src/main/java/com/jepl/Main.java
+++ todo-challenge/src/main/java/com/jepl/Main.java
@@ -1,14 +1,19 @@
 package com.jepl;
 
 import com.google.common.base.*;
+import com.google.inject.*;
+import com.google.inject.servlet.*;
+
+import com.jepl.configs.*;
 
 import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.*;
 import org.eclipse.jetty.webapp.*;
 import org.slf4j.*;
 
-import java.io.*;
-import java.net.*;
 import java.util.*;
+
+import javax.servlet.*;
 
 public class Main {
     static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -17,9 +22,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         int port = 8080;
-        String portString = System.getProperty("port");
-        if (!Strings.isNullOrEmpty(portString)) {
+        if (args.length > 0 && !Strings.isNullOrEmpty(args[0])) {
             try {
+                String portString = args[0].split("=")[1];
                 port = Integer.parseInt(portString);
             } catch (Exception e) {
                 logger.error("Can't parse String to Integer for port number. {}", e);
@@ -28,9 +33,14 @@ public class Main {
 
         server = new Server(port);
         context = new WebAppContext();
-        context.setResourceBase(Main.class.getClass().getResource("/").toString());
-        context.setDescriptor(Main.class.getResource("/web.xml").toString());
+        context.setResourceBase("/");
         context.setContextPath("/");
+        ServletHandler handler = new ServletHandler();
+        FilterHolder filterHolder = handler.addFilterWithMapping(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.INCLUDE, DispatcherType.REQUEST));
+        filterHolder.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+//        handler.addServletWithMapping(TestServlet.class, "/*");
+        context.addEventListener(new GuiceServletConfig());
+        context.setServletHandler(handler);
         context.setParentLoaderPriority(true);
 
         server.setHandler(context);
